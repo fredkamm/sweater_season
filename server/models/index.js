74aebfad7d6324d2335efa30564353a400299a64
Northern Pines <traviswalborn8@gmail.com>
2022-09-27 14:06:31 -0400

    model setup

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Sweater = require('./Sweater');
+const Tag = require('./Tag');
+const Order = require('./Order');
+
+module.exports = { User, Sweater, Tag, Order };

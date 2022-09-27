c4e38bb2b62b349f4dd3e1dd585ef689a965e5f1
jlamond37 <jlamond37@gmail.com>
2022-09-27 13:55:44 -0400

    react bootstrap setup

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
